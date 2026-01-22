66ea97e44768f584174d9d09434c027cbd141dc2
Everton Nikolas de Oliveira <enikolas.oliveira@gmail.com>
2026-01-22 00:09:04 -0300

    chore: add CI pipeline with gradle build and tests

--- src/test/java/io/github/enikolas/taskmanagement/TaskManagementApiApplicationTests.java
+++ src/test/java/io/github/enikolas/taskmanagement/TaskManagementApiApplicationTests.java
@@ -2,9 +2,25 @@ package io.github.enikolas.taskmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
+@Testcontainers
 @SpringBootTest
 class TaskManagementApiApplicationTests {
+
+	@Container
+	static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:18.1");
+
+	@DynamicPropertySource
+	static void overrideProps(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
