af1157a52ad60f2acc8bf60adf265da8090da764
Everton Nikolas de Oliveira <enikolas.oliveira@gmail.com>
2026-01-21 23:16:58 -0300

    docs: add initial Swagger setup

---
+++ src/main/java/io/github/enikolas/taskmanagement/infra/springdoc/SpringDocConfigurations.java
@@ -0,0 +1,22 @@
+package io.github.enikolas.taskmanagement.infra.springdoc;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfigurations {
+    @Bean
+    public OpenAPI taskManagementOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Task Management API")
+                        .description("Task Management API is a RESTful service inspired by Trello, designed to " +
+                                "manage boards, lists, and cards.")
+                        .license(new License()
+                                .name("MIT")
+                                .url("https://github.com/enikolas/task-management-api/blob/main/LICENSE")));
+    }
+}
