4bebf65232dc1b410dc45ac71a3e4d257fe860e0
Everton Nikolas de Oliveira <enikolas.oliveira@gmail.com>
2026-01-24 12:16:53 -0300

    chore: apply clean architecture modular structure

--- bootstrap/src/main/java/io/github/enikolas/taskmanagement/bootstrap/TaskManagementApiApplication.java
+++ bootstrap/src/main/java/io/github/enikolas/taskmanagement/bootstrap/TaskManagementApiApplication.java
@@ -1,4 +1,4 @@
-package io.github.enikolas.taskmanagement;
+package io.github.enikolas.taskmanagement.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TaskManagementApiApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(TaskManagementApiApplication.class, args);
 	}
 
--- bootstrap/src/test/java/io/github/enikolas/taskmanagement/bootstrap/TaskManagementApiApplicationTests.java
+++ bootstrap/src/test/java/io/github/enikolas/taskmanagement/bootstrap/TaskManagementApiApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.enikolas.taskmanagement;
+package io.github.enikolas.taskmanagement.bootstrap;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
